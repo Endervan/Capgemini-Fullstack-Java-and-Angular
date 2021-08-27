85a1928dbe701b0d3291aab46a529dfe40790413
endva <endvan@gmail.com>
2021-08-27 08:18:36 -0300

    InferenciaExemplo

---
+++ src/funcoesExemplo/InferenciaExemplo.java
@@ -0,0 +1,19 @@
+package funcoesExemplo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.stream.Collectors;
+
+public class InferenciaExemplo {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://docs.oracle.com/javase/10/language");
+        URLConnection urlConnection = url.openConnection();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+        System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">",">\n"));
+    }
+
+}
