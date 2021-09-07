a8483e43450e0167dd5a90b8c4eaa393a404f766
endva <endvan@gmail.com>
2021-09-07 09:14:56 -0300

    Conhecendo os métodos Map - parte 2

--- src/metodos/map/ExemploMapPart01.java
+++ src/metodos/map/ExemploMapPart01.java
@@ -2,6 +2,7 @@ package metodos.map;
 
 import com.sun.source.doctree.SeeTree;
 
+import javax.crypto.spec.PSource;
 import java.security.cert.CollectionCertStoreParameters;
 import java.util.*;
 
@@ -35,16 +36,61 @@ public class ExemploMapPart01 {
         System.out.println("Exiba os CONSUMOS DOS CARROS " + carrosPopulares.values()); // apenas os valores
 
         System.out.println("Exiba carro o modelo mais eficiente ");
-
         Double consumoMaisEficiente = Collections.max(carrosPopulares.values());
-        Set<Map.Entry<String, Double>> entries = carrosPopulares.entrySet();// trabalhar com chave e valores
-       String modeloMaisEficiente = "";
-       for (Map.Entry<String, Double> entry:entries){
-           if (entry.getValue().equals(consumoMaisEficiente)) {
-               modeloMaisEficiente = entry.getKey();
-               System.out.println("modelo mais eficiente : "+ modeloMaisEficiente +" - " + consumoMaisEficiente);
-           }
-       }
+        Set<Map.Entry<String, Double>> entries = carrosPopulares.entrySet();// trabalhar com chave e valores separadamente
+        String modeloMaisEficiente = "";
+        for (Map.Entry<String, Double> entry : entries) {
+            if (entry.getValue().equals(consumoMaisEficiente)) {
+                modeloMaisEficiente = entry.getKey();
+                System.out.println("modelo mais eficiente : " + modeloMaisEficiente + " - " + consumoMaisEficiente);
+            }
+        }
 
+        System.out.println("Exiba carro o modelo menos eficiente ");
+        Double consumoMenosEficiente = Collections.min(carrosPopulares.values());
+        String modeloMenosEficiente = "";
+
+        for (Map.Entry<String, Double> entry : carrosPopulares.entrySet()) {
+            if (entry.getValue().equals(consumoMenosEficiente)) {
+                modeloMenosEficiente = entry.getKey();
+                System.out.println("Modelo menos eficiente " + modeloMenosEficiente + " - " + consumoMenosEficiente);
+            }
+        }
+
+        Iterator<Double> iterator = carrosPopulares.values().iterator();
+        Double soma = 0d;
+        while (iterator.hasNext()) {
+            soma += iterator.next();
+        }
+        System.out.println("Exiba a soma dos consumos " + soma);
+
+
+        System.out.println("Exiba a media dos consumos " + (soma / carrosPopulares.size()));
+
+
+        carrosPopulares.values().removeIf(aDouble -> aDouble.equals(15.6));
+        System.out.println("Remova os modelos com consumo igual a 15,6 km/l "+ carrosPopulares);
+        // ouuuuuuuuuu
+//        while (iterator1.hasNext()) {
+//            if (iterator1.next().equals(15.6)) iterator1.remove();
+//        }
+
+        Map<String, Double> carrosPopulares1 = new LinkedHashMap<>() {{ // ordem de inserção
+            put("Gol", 14.4);
+            put("Uno", 15.6);
+            put("Mobi", 16.1);
+            put("Kwid", 13.4);
+            put("H20", 19.4);
+        }};
+        System.out.println("Exibar todos os carros na ordem que foram informadas " + carrosPopulares1);
+
+
+        Map<String, Double> carrosPopulares2 = new TreeMap<>(carrosPopulares1);// ordena chaves ordem alfabetica
+
+        System.out.println("Exiba o dicionario na ordem pelo modelo " + carrosPopulares2 );
+
+        carrosPopulares.clear();
+        System.out.println("Apague o dicionario de carros " + carrosPopulares);
+        System.out.println("Confira se o dicionario esta vazio " + carrosPopulares.isEmpty());
     }
 }
