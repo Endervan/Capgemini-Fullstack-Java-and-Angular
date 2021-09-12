18bbd3d951825ffa91bfa73ec909376fe0b29efb
endva <endvan@gmail.com>
2021-09-12 19:44:19 -0300

    desafios java basico: fatorial e divisores

---
+++ src/teste/java/endervan/Divisores.java
@@ -0,0 +1,20 @@
+package teste.java.endervan;
+
+import java.util.Scanner;
+
+public class Divisores {
+    //complete os espaços em branco com sua solução
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        n = input.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println(n);
+    }
+
+}
