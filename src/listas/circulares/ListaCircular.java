ff1ed99bd0c76918ad0595dbc4adacab39a1a4fe
endva <endvan@gmail.com>
2021-10-02 17:13:30 -0300

    Implementação da classe ListaCircular (remove())

--- src/listas/circulares/ListaCircular.java
+++ src/listas/circulares/ListaCircular.java
@@ -6,6 +6,30 @@ public class ListaCircular<T> {
     private No<T> cauda;
     private int tamanhoLista;
 
+    public void remove(int index) {
+        if (index >= this.tamanhoLista) throw new IndexOutOfBoundsException("o indice e maior que o tamanho da lista");
+
+        No<T> noAuxilar = cauda;
+        if (index == 0) {
+            this.cauda = this.cauda.getNoProximo();
+            this.cabeca.setNoProximo(this.cauda);
+        } else if (index == 1) {
+            this.cauda.setNoProximo(this.cauda.getNoProximo());
+        } else {
+            for (int i = 0; i < index - 1; i++) {
+                noAuxilar = noAuxilar.getNoProximo();
+            }
+
+            noAuxilar.setNoProximo(noAuxilar.getNoProximo().getNoProximo());
+        }
+
+        this.tamanhoLista--;
+    }
+
+    public T get(int index) {
+        return this.getNo(index).getConteudo();
+    }
+
     private No<T> getNo(int index) {
         if (this.isEmpty()) throw new IndexOutOfBoundsException("A lista esta vazia");
         if (index == 0) return this.cauda;
