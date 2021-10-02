bf3d309ab5660f6ddba7fe242054b97f7eb6b648
endva <endvan@gmail.com>
2021-10-02 12:31:31 -0300

    Implementação da classe ListaCircular (getNo() e get())

--- src/listas/circulares/ListaCircular.java
+++ src/listas/circulares/ListaCircular.java
@@ -3,8 +3,18 @@ package listas.circulares;
 public class ListaCircular<T> {
 
     private No<T> cabeca;
-    private No<T> calda;
+    private No<T> cauda;
     private int tamanhoLista;
+
+    private No<T> getNo(int index) {
+        if (this.isEmpty()) throw new IndexOutOfBoundsException("A lista esta vazia");
+        if (index == 0) return this.cauda;
+        No<T> noAuxiliar = this.cauda;
+        for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
 
     public boolean isEmpty() {
         return this.tamanhoLista == 0 ? true : false;
