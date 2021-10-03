7dff6d01e6feac99c0014ad8920104333c42f416
endva <endvan@gmail.com>
2021-10-03 12:36:53 -0300

    Implementação da classe ArvoreBinaria - Parte 7

--- src/arvores/binarias/ArvoreBinaria.java
+++ src/arvores/binarias/ArvoreBinaria.java
@@ -103,10 +103,37 @@ public class ArvoreBinaria<T extends Comparable<T>> {
                     raiz = filho;
                 }
             } else if (atual.getNoDir() == null) {
-
+                if (pai.getNoEsq() == atual) {
+                    pai.setNoEsq(atual.getNoEsq());
+                } else {
+                    pai.setNoDir(atual.getNoEsq());
+                }
             } else if (atual.getNoEsq() == null) {
-
+                if (pai.getNoEsq() == atual) {
+                    pai.setNoEsq(atual.getNoDir());
+                } else {
+                    pai.setNoDir(atual.getNoDir());
+                }
             } else {
+
+                for (
+                        temp = atual, filho = atual.getNoEsq();
+                        filho.getNoDir() != null;
+                        temp = filho, filho.getNoDir()
+                ) {
+
+                    if (filho != atual.getNoEsq()) {
+                        temp.setNoDir(filho.getNoEsq());
+                        filho.setNoEsq(atual.getNoEsq());
+                    }
+                    filho.setNoDir(atual.getNoDir());
+                    if (pai.getNoEsq() == atual) {
+                        pai.setNoEsq(filho);
+                    } else {
+                        pai.setNoDir(filho);
+                    }
+
+                }
 
             }
 
