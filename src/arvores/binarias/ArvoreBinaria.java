4ce81562c58e66efa82d5298a2751300751aea3f
endva <endvan@gmail.com>
2021-10-02 21:43:06 -0300

    Implementação da classe ArvoreBinaria - Parte 1Implementação da classe ArvoreBinaria - Parte 1

---
+++ src/arvores/binarias/ArvoreBinaria.java
@@ -0,0 +1,27 @@
+package arvores.binarias;
+
+public class ArvoreBinari0a<T extends Comparable<T>> {
+
+    private BinNo<T> raiz;
+
+    public void ArvoreBinaria() {
+        this.raiz = null;
+    }
+
+    public void inserir(T conteudo) {
+        BinNo<T> novoNo = new BinNo<>(conteudo);
+        raiz = inserir(raiz, novoNo);
+    }
+
+    private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo) {
+        if (atual == null) {
+            return novoNo;
+        } else if (novoNo.getConteudo().compareTo(atual.getConteudo()) < 0) {
+            atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+        } else {
+            atual.setNoDir(inserir(atual.getNoDir(), novoNo));
+        }
+
+        return atual;
+    }
+}
---
+++ src/arvores/binarias/BinNo.java
@@ -0,0 +1,47 @@
+package arvores.binarias;
+
+public class BinNo<T extends Comparable<T>> {
+
+    private T conteudo;
+    private BinNo<T> noEsq;
+    private BinNo<T> noDir;
+
+    public BinNo(T conteudo) {
+        this.conteudo = conteudo;
+        noEsq = noDir = null;
+    }
+
+    public BinNo() {
+    }
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public BinNo<T> getNoEsq() {
+        return noEsq;
+    }
+
+    public void setNoEsq(BinNo<T> noEsq) {
+        this.noEsq = noEsq;
+    }
+
+    public BinNo<T> getNoDir() {
+        return noDir;
+    }
+
+    public void setNoDir(BinNo<T> noDir) {
+        this.noDir = noDir;
+    }
+
+    @Override
+    public String toString() {
+        return "BinNo{" +
+                "conteudo=" + conteudo +
+                '}';
+    }
+}
