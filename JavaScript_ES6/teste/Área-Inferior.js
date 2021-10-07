70f43fadc82de3119ca38f12b0d321c377ab42a0
endva <endvan@gmail.com>
2021-10-06 23:11:41 -0300

    area inferior

--- JavaScript_ES6/teste/Área-Inferior.js
+++ JavaScript_ES6/teste/Área-Inferior.js
@@ -1,68 +1,113 @@
-// a função gets é implementada dentro do sistema para ler
-// as entradas(inputs) dos dados
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados
 // Abaixo segue um exemplo de código que você pode ou não utilizar
 
-// const isSum = lines.shift() === "S";
-//
-// const xL = 12,
-//     yL = 12;
-//
-// let sum = 0.0,
-//     av = 0.0;
-//
-// for (                   ) {
-//     for (                  ) {
-//         const i = Number(lines.shift());
-//
-//         if (                    ) {
-//             sum += i;
-//             ++av;
-//         }
-//     }
-// }
-//
-// console.log((sum / (isSum ? 1 : av)).toFixed("1"));
-//
-// var a = ['c','g','ga']
-//
-// a[100] = 'r';
-// console.log(a.length)
-//
-// class tax {
-//     calculo static
-// }
+const operacao = gets();
 
-// const foo = {nome:"a"};
-//  delete foo.nome;
-//
-// console.log(foo)
+const isSum = operacao === "S";
 
-// let r = ['a','b','c','d','e',]
-//
-// r.splice(0,2);
-// console.log(r)
 
-// const x = 6%2;
-// const y = x ? 'one':'two';
-// console.log(y)
 
-// console.log('dirt wa'.split('dirt').length)
+const xL = 12, yL = 12;
 
-// let a = 5;
-// console.log(a++)
-// console.log(++a)
+let sum = 0.0, av = 0.0;
 
-// const m = ()=>{
-//     const  a = 2;
-//     return ()=> console.log('a' + a);
-// }
-//
-// const a = 1;
 
-// const o ={
-//     a:1,
-//     b:2,
-//     xc:3,
-// }
 
+let linha = gets();
+
+
+
+if (linha.length > 5) {
+
+   for (var y = 0; y < yL; y++) {
+
+
+
+    if (y != 0) {
+
+      linha = gets();
+
+    }
+
+    let numeros = linha.trim().split(/[\s,]+/);
+
+
+
+    for (var x = 0; x < xL; x++) {
+
+
+
+      if ((y > 6) && (x < y) && (x > 11 - y)) {
+
+        var valor = Number(numeros[x]);
+
+        sum += valor;
+
+        ++av;
+
+      }
+
+    }
+
+  }
+
+}
+
+else {
+
+
+
+  var contagem = 1;
+
+  for (var y = 0; y < yL; y++) {
+
+
+
+    for (var x = 0; x < xL; x++) {
+
+
+
+      var valor = 0;
+
+
+
+      if (contagem == 1) {
+
+        valor = Number(linha);
+
+        primeiroloop = false;
+
+      }
+
+      else {
+
+        linha = gets().trim();
+
+        valor = Number(linha);
+
+      }
+
+
+
+      if ((y > 6) && (x < y) && (x > 11 - y)) {
+
+        sum += valor;
+
+        ++av;
+
+      }
+
+
+
+      contagem++
+
+    }
+
+  }
+
+}
+
+
+
+console.log((sum / (isSum ? 1 : av)).toFixed("1"));
 
