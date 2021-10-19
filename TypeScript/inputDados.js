1cd4aaad6bd43c63407ca5bcb7db22937a6b439e
endva <endvan@gmail.com>
2021-10-18 21:56:12 -0300

    strikes

---
+++ TypeScript/inputDados.js
@@ -0,0 +1,7 @@
+// as HTMLInputElement propriedade pega tudo input existe outro formatos tb
+// const input = document.getElementById('input') as HTMLInputElement;
+// input.addEventListener('input', (event) => {
+//     const i = event.currentTarget as HTMLInputElement;
+//     console.log(i.value)
+// });
+//# sourceMappingURL=inputDados.js.map
