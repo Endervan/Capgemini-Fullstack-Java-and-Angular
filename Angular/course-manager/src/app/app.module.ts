4a6f69979469ce3fcb3d6d91c6368a5e249c90e4
edvan.ximenes <endvan@gmail.com>
2021-10-22 11:22:43 -0300

    pasta core=> regra de negocio pra modulos mais pesados

--- Angular/course-manager/src/app/app.module.ts
+++ Angular/course-manager/src/app/app.module.ts
@@ -3,22 +3,23 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
-import {NavBarComponent} from "./nav-bar/nav-bar-component";
+import {NavBarComponent} from "./core/component/nav-bar/nav-bar-component";
 import {RouterModule} from "@angular/router";
 import {Erro404Component} from "./error-404/erro404.component";
 import {HttpClientModule} from "@angular/common/http";
 import {CourseModule} from "./courses/course.module";
+import {CoreModule} from "./core/component/core.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Erro404Component,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
---
+++ Angular/course-manager/src/app/core/component/core.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from "@angular/core";
+import {NavBarComponent} from "./nav-bar/nav-bar-component";
+import {RouterModule} from "@angular/router";
+
+
+@NgModule({
+  declarations: [
+    NavBarComponent
+  ],imports:[
+    RouterModule
+  ], exports: [
+    NavBarComponent
+  ]
+})
+export class CoreModule {
+
+}
