8ac35f0e35457c8551a64e3a1d08fe4415a2dd32
edvan.ximenes <endvan@gmail.com>
2021-10-23 18:41:27 -0300

    Criando um formulário

---
+++ Angular/Avancado/angular-filmes/src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro-filmes',
+  templateUrl: './cadastro-filmes.component.html',
+  styleUrls: ['./cadastro-filmes.component.scss']
+})
+export class CadastroFilmesComponent implements OnInit {
+
+  cadastro: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+
+    this.cadastro = this.fb.group({
+      titulo: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      urlFoto: ['', [Validators.minLength(10)]],
+      dtLacamento: ['', [Validators.required]],
+      descricao: [''],
+      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDb: ['', [Validators.minLength(10)]],
+      genero: ['', [Validators.required]],
+    });
+
+  }
+
+  salva(): void {
+    if (this.cadastro.invalid) {
+      return;
+    }
+    alert(JSON.stringify(this.cadastro.value, null, 4));
+  }
+
+  reiniciarForm(): void {
+    this.cadastro.reset();
+  }
+}
