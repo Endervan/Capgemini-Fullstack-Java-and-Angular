cf6fd90cd7b6e04e36b892465d67cf8dbc93c00d
edvan.ximenes <endvan@gmail.com>
2021-10-23 23:47:33 -0300

    Deixando as mensagens de erros dinâmicas

--- Angular/Avancado/angular-filmes/src/app/shared/components/campos/validar-campos.service.ts
+++ Angular/Avancado/angular-filmes/src/app/shared/components/campos/validar-campos.service.ts
@@ -9,15 +9,23 @@ export class ValidarCamposService {
   constructor() {
   }
 
-  hasErrorValidate(control: AbstractControl, errName: string){
-    if ((control.touched || control.dirty) && this.hasError(control,errName)){
+  // trata erros gerais
+  hasErrorValidate(control: AbstractControl, errName: string) {
+    if ((control.touched || control.dirty) && this.hasError(control, errName)) {
       return true;
     }
-    return  false;
+    return false;
   }
 
   // tratamento error personalizados
   hasError(control: AbstractControl, errName: string): boolean {
     return control.hasError(errName);
+  }
+
+  // tratamento error personalizados
+  lenghtValidar(control: AbstractControl, errName: string): number {
+    const error = control.errors[errName];
+    return error.requiredLenght || error.min || error.max || 0;
+
   }
 }
